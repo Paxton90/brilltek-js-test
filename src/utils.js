ff6f51e5146a59a2e1a0a86c0ad635a9b26878d4
Paxton90 <a22537743@gmail.com>
2023-07-03 17:20:59 +0800

    implement isTimeBetween

--- src/utils.js
+++ src/utils.js
@@ -12,13 +12,35 @@ function hoursDifference(startTime, endTime) {
     if (endTime === "00:00:00") {
         endDate = new Date('2023-07-04T00:00:00')
     }
-    
+
     const timeDifference = endDate.getTime() - startDate.getTime()
     const hoursDifference = Math.abs(timeDifference) / (1000 * 60 * 60)
 
     return hoursDifference
 }
 
+/**
+ * Calculate diffrence by hour
+ * @param {string} startTime
+ * @param {string} endTime
+ * @param {string} dayTime
+ * @returns {boolean}
+ */
+function isTimeBetween(startTime, endTime, dayTime) {
+    let startDate = new Date('2023-07-03T'.concat(startTime))
+    let endDate = new Date('2023-07-03T'.concat(endTime))
+    let time = new Date('2023-07-03T'.concat(dayTime))
+
+    // hours in day are cycling
+    if (endTime === "00:00:00") {
+        endDate = new Date('2023-07-04T00:00:00')
+    }
+
+    return time >= startDate && time <= endDate;
+}
+
+
 module.exports = {
-    hoursDifference: hoursDifference
+    hoursDifference: hoursDifference,
+    isTimeBetween: isTimeBetween
 }
