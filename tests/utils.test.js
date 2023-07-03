e90099ee35239609bd9778ae75c9eb9cfa0f6eee
Paxton90 <a22537743@gmail.com>
2023-07-03 17:20:40 +0800

    add isTimeBetween test case

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,4 @@
-const { hoursDifference } = require('../src/utils')
+const { hoursDifference, isTimeBetween } = require('../src/utils')
 
 
 // Test hoursDifference
@@ -9,4 +9,16 @@ test('Calculate diffrence by hour', () => {
     const hoursDiff = hoursDifference(work_begin, work_end)
 
     expect(hoursDiff).toEqual(4)
+})
+
+// Test isTimeBetween
+test('isTimeBetween return true if time between startTime and endTime', () => {
+    const start_time = "20:00:00"
+    const end_time = "00:00:00"
+    
+    const time1 = "22:00:00"
+    const time2 = "18:00:00"
+
+    expect(isTimeBetween(start_time, end_time, time1)).toEqual(true)
+    expect(isTimeBetween(start_time, end_time, time2)).toEqual(false)
 })
