f4b85af8ec2f651f8ae0db3fcdd4c713c372cdb2
Paxton90 <a22537743@gmail.com>
2023-07-03 18:04:29 +0800

    implement calculateNeededDays

--- src/tasks.js
+++ src/tasks.js
@@ -1,5 +1,25 @@
-function calculateNeededDays(){
-    //How many days of work needed to done all tasks ? // => 1 day = 9:00 to 00:00 between 00:00 and 09:00 doesnt count.
+const { countTotalHoursWorked } = require('./employees')
+
+
+/**
+ * Count Employees Number by Factory
+ * @param {Array} employeeType
+ * @param {Array} employees
+ * @param {Array} tasks
+ * @returns {number}
+ */
+function calculateNeededDays(employeeType, employees, tasks){
+    const workingHoursPerDay = countTotalHoursWorked(employeeType, employees)
+
+    // calculate total taskHours
+    let taskMins = 0
+    tasks.forEach(task => {
+        taskMins += task.duration
+    })
+
+    // needed day = taskHours divided by all employees total worked hours in a day
+    const neededDays = (taskMins/60)/workingHoursPerDay
+    return neededDays
 }
 
 module.exports = {
