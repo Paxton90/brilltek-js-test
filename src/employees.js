70bf22b7f08016631637c429d40503b02ef343d5
Paxton90 <a22537743@gmail.com>
2023-07-03 13:21:23 +0800

    Modularization

---
+++ src/employees.js
@@ -0,0 +1,7 @@
+export function countTotalHoursWorked() {
+    // Count total hours worked in 1 day ? // => 39
+}
+
+export function howManyEmplyeeBytime() {
+    // Make a function that take as parameters dayTime and return number of employee working // howManyEmployeeByTime(time) => int
+}
---
+++ src/factories.js
@@ -0,0 +1,11 @@
+export function countEmployeesByFactory() {
+    // Count Employees Number by Factory // => [ {name: 'BR1', count: 4}, ... ]
+}
+
+export function countFactoriesByEmployee() {
+    // Count Factories Number by Employee // => [ {employee: 'John', count: 2}, ... ]
+}
+
+export function orderEmployeesAlphabetically() {
+    // Order employees list by alphabetical order // => { name: "BR2", employees: ["Jessie", "John", "Karen"] }
+}
