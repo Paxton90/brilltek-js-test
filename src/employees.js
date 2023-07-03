c994d5a112a71fdfa92570dd366d888ce6ef21e5
Paxton90 <a22537743@gmail.com>
2023-07-03 16:49:52 +0800

    add comments

--- src/employees.js
+++ src/employees.js
@@ -8,11 +8,13 @@ const { hoursDifference } = require('./utils')
  * @returns {number}
  */
 function countTotalHoursWorked(employeeType, employees) {
+    // Calculate workingHours every employeeType
     const workingHours = {}
     employeeType.forEach(type => {
         workingHours[type.id] = hoursDifference(type.work_begin, type.work_end)
     })
 
+    // Add all employee work time
     let totalHoursWorked = 0
     employees.forEach(employee => {
         totalHoursWorked += workingHours[employee.type]
